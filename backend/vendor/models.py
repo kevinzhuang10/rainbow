d6bd33fcff0d049bd677b781f5e8b501cd8de033
Kevin Huang <kevinzhuang10@gmail.com>
2020-02-08 18:28:36 -0800

    vendor model

--- backend/vendor/models.py
+++ backend/vendor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Vendor(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    link = models.CharField(max_length=200)
+    image = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
