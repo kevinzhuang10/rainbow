4414885b67aaaa40c63ca2c84e2796feb0a1f830
Kevin Huang <kevinzhuang10@gmail.com>
2020-02-09 17:35:14 -0800

    remove husky and lint-staged

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import toDoApp from './reducers';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { loadToDoList } from './actions';
+import toDoApp from './reducers';
+import rootSaga from './sagas';
 
-const store = createStore(toDoApp);
+const sagaMiddleware = createSagaMiddleware();
 
-ReactDOM.render(
-<Provider store={store}>
-  <App />
-</Provider>, 
-document.getElementById('root'));
+const store = createStore(toDoApp, applyMiddleware(sagaMiddleware));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+sagaMiddleware.run(rootSaga);
+
+store.dispatch(loadToDoList());
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
